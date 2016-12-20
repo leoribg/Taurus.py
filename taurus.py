0c057379d61295a3dee7d25c9c806bfbf50bcf3e
Leonardo <leonardo.ribeiro@gee.inatel.br>
2016-12-20 10:54:24 -0200

    Update taurus.py

--- taurus.py
+++ taurus.py
@@ -8,14 +8,14 @@ def taurus(serial):
 def write_dActuator(actID, value):
     url = 'http://update.taurussystem.com/ws/webresources/Digital/set?key=%s&actuator=%s&value=%s' % (_serial, actID, value)
     r=requests.get(url)
-    print r.status_code
+    #print r.status_code
     #print r.headers
     #print r.content
 
 def read_dActuator(actID):
     url = 'http://update.taurussystem.com/ws/webresources/Digital/get?key=%s&&actuator=%s' % (_serial, actID)
     r=requests.get(url)
-    print r.status_code
+    #print r.status_code
     #print r.headers
     #print r.content
     response = json.loads(r.content)
@@ -24,14 +24,14 @@ def read_dActuator(actID):
 def write_aActuator(actID, value):
     url = 'http://update.taurussystem.com/ws/webresources/Analog/set?key=%s&actuator=%s&value=%s' % (_serial, actID, value)
     r=requests.get(url)
-    print r.status_code
-    print r.headers
-    print r.content
+    #print r.status_code
+    #print r.headers
+    #print r.content
 
 def read_aActuator(actID):
     url = 'http://update.taurussystem.com/ws/webresources/Analog/get?key=%s&&actuator=%s' % (_serial, actID)
     r=requests.get(url)
-    print r.status_code
+    #print r.status_code
     #print r.headers
     #print r.content
     response = json.loads(r.content)
@@ -40,14 +40,14 @@ def read_aActuator(actID):
 def write_sensor(sensorID, value):
     url = 'http://update.taurussystem.com/ws/webresources/Sensor/set?key=%s&sensor=%s&value=%s' % (_serial, sensorID, value)
     r=requests.get(url)
-    print r.status_code
-    print r.headers
-    print r.content
+    #print r.status_code
+    #print r.headers
+    #print r.content
 
 def read_sensor(sensorID):
     url = 'http://update.taurussystem.com/ws/webresources/Sensor/get?key=%s&&sensor=%s' % (_serial, sensorID)
     r=requests.get(url)
-    print r.status_code
+    #print r.status_code
     #print r.headers
     #print r.content
     response = json.loads(r.content)
